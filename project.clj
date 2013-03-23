482d953a81acb06f38ef8314bedbaab3a4fec01d
Wolfe Styke <wstyke@mit.edu>
2013-03-23 13:08:18 -0700

    Initial commit of bare compojure server for DOM Prohpet app.

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject dom-prophet "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [compojure "1.1.5"]]
+  :plugins [[lein-ring "0.8.2"]]
+  :ring {:handler dom-prophet.handler/app}
+  :profiles
+  {:dev {:dependencies [[ring-mock "0.1.3"]]}})
---
+++ src/dom_prophet/handler.clj
@@ -0,0 +1,11 @@
+(ns dom-prophet.handler
+  (:use compojure.core)
+  (:require [compojure.handler :as handler]
+            [compojure.route :as route]))
+
+(defroutes app-routes
+  (GET "/" [] "Hello World")
+  (route/not-found "Not Found"))
+
+(def app
+  (handler/site app-routes))
